b4c82de1954042c09a91021b1659bfdebb7b30e3
dta613 <dta613@gmail.com>
2017-06-21 00:33:37 -0400

    Making changes on admin page for display

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -3,6 +3,6 @@ from models import Patient
 # Register your models here.
 
 class PatientAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('Patient_name', 'age', 'location', 'condition', 'medication', 'initial_visit', 'followup_appt', 'reminder_freq')
 
 admin.site.register(Patient, PatientAdmin)

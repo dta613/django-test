3def0cf57ac5452152027f2abc752d9a0b2081ea
dta613 <dta613@gmail.com>
2018-02-19 13:00:22 -0500

    Update settings.py

--- django_test/settings.py
+++ django_test/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['django-testing-v1.herokuapp.com', 'localhost']
 
 
 # Application definition

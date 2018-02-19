c7d2ed5750bbdb8dd5f529bb11b7e3b5f7b12817
D <dt@Ds-MacBook-Pro.local>
2018-02-19 15:13:08 -0500

    Removed abcpmc, and add a comma for urls.py

--- django_test/urls.py
+++ django_test/urls.py
@@ -43,7 +43,7 @@ router.register(r'users', UserViewSet)
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 	url(r'^login/', auth_views.login, name='login'),
     url(r'^logout/$', users_views.logout, name='logout'),
     url(r'^signup/', users_views.signup, name='signup'),
@@ -52,5 +52,5 @@ urlpatterns = [
     url(r'^list/$', dashboard_views.Patient_list.as_view(), name='list'),
     url(r'^$', users_views.index, name='index'),
 
-    
+
 ]

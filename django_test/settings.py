abe9d86d7d04883210325e4fadad03ed64f0775d
Danny Tsoi <dta613@gmail.com>
2019-02-17 19:32:53 -0500

    Update settings.py

--- django_test/settings.py
+++ django_test/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['dawa-test.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost']
 
 
 # Application definition
